e43c41e4b45f35acb4ef8208043ed97406135414
ssongting <49968982+ssongting@users.noreply.github.com>
2023-07-12 03:39:04 +0900

    feat: worldmap 셰이더 추가
    월드맵 전용 셰이더를 만들었어요 원래 fbx 로드할때 셰이더 장착되는 기본값이 std3ddeffered셰이더인데,월드맵 로드할때만 fbxLoader에서 해당 값을 worldmap셰이더로 바꿔주면 될꺼같아요 아직 월드맵 적용은 안하고 셰이더만 일단 업로드합니다!

--- Project/Engine/CResMgr_init.cpp
+++ Project/Engine/CResMgr_init.cpp
@@ -1255,6 +1255,32 @@ void CResMgr::CreateDefaultGraphicsShader()
 
 	AddRes<CGraphicsShader>(L"DepthMapShader", pShader);
 
+
+
+	// ============================
+	// worldmap Shader
+	// RS_TYPE : CULL_BACK
+	// DS_TYPE : LESS
+	// BS_TYPE : DEFAULT	 
+	// Domain : DOMAIN_UNDEFINED
+	// ============================
+	pShader = new CGraphicsShader;
+	pShader->SetKey(L"worldmapShader");
+	pShader->CreateVertexShader(L"shader\\worldmap.fx", "VS_worldmap_Deferred");
+	pShader->CreatePixelShader(L"shader\\worldmap.fx", "PS_worldmap_Deferred");
+
+	pShader->SetRSType(RS_TYPE::CULL_BACK);
+	pShader->SetDSType(DS_TYPE::LESS);
+	pShader->SetBSType(BS_TYPE::ALPHA_BLEND);
+
+	pShader->SetDomain(SHADER_DOMAIN::DOMAIN_DEFERRED);
+
+	// Param	
+	pShader->AddTexParam(TEX_0, "Output Texture");
+	pShader->AddTexParam(TEX_1, "Normal Texture");
+
+	AddRes(pShader->GetKey(), pShader);
+
 }
 
 
