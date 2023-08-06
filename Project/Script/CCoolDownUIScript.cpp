72501dbd6739f47137e5c138c3bdcf0931495e94
ssongting <49968982+ssongting@users.noreply.github.com>
2023-08-06 16:24:47 +0900

    feat:UI연동 중간커밋 - 미니맵UI연동,개별캐릭터 패널 HPMP패널 연동

--- Project/Script/CCoolDownUIScript.cpp
+++ Project/Script/CCoolDownUIScript.cpp
@@ -31,98 +31,87 @@ void CCoolDownUIScript::begin()
 
 void CCoolDownUIScript::tick()
 {
-	if (m_SkillType == COOL_DOWN_TYPE::Q)
+	CUnitScript* UnitInfo = CSendServerEventMgr::GetInst()->GetMyPlayer()->GetScript<CUnitScript>();
+
+	if (m_SkillType == COOL_DOWN_TYPE::Q && UnitInfo->GetSkill(1))
 	{
-		CUnitScript* UnitInfo = CSendServerEventMgr::GetInst()->GetMyPlayer()->GetScript<CUnitScript>();
 		m_fCooldownTime = UnitInfo->GetSkill(1)->GetCoolDown();
 		m_fCurCoolTime = UnitInfo->GetSkill(1)->GetCurCoolDown();
 	}
 
-	if (m_SkillType == COOL_DOWN_TYPE::W)
+	if (m_SkillType == COOL_DOWN_TYPE::W && UnitInfo->GetSkill(2))
 	{
-
+		m_fCooldownTime = UnitInfo->GetSkill(2)->GetCoolDown();
+		m_fCurCoolTime = UnitInfo->GetSkill(2)->GetCurCoolDown();
 	}
 
-	if (m_SkillType == COOL_DOWN_TYPE::E)
+	if (m_SkillType == COOL_DOWN_TYPE::E && UnitInfo->GetSkill(3))
 	{
-
+		m_fCooldownTime = UnitInfo->GetSkill(3)->GetCoolDown();
+		m_fCurCoolTime = UnitInfo->GetSkill(3)->GetCurCoolDown();
 	}
 
-	if (m_SkillType == COOL_DOWN_TYPE::R)
+	if (m_SkillType == COOL_DOWN_TYPE::R && UnitInfo->GetSkill(4))
 	{
-
+		m_fCooldownTime = UnitInfo->GetSkill(4)->GetCoolDown();
+		m_fCurCoolTime = UnitInfo->GetSkill(4)->GetCurCoolDown();
 	}
-	//===========디버깅용================
-	if (KEY_TAP(KEY::F7) && !m_bSkillUse)
+
+	if (m_fCurCoolTime <= 0)
 	{
+		m_fCoolRatio = 0.f;  // 쿨타임 종료, 0 설정
+		m_bSkillUse = false;
+	}
+	else
+	{
+		m_fCoolRatio = m_fCurCoolTime / m_fCooldownTime;// 쿨타임 중, 쿨타임의 진행 비율을 계산하여 설정
 		m_bSkillUse = true;
 	}
 
-	if (!m_bSkillUse)
-	{
-		m_fCoolRatio = 0.0f; // 쿨타임 없음, 0 설정
-		m_fCurCoolTime = m_fCooldownTime;
-	}
-
-	if (m_bSkillUse)
-	{
-		m_fCurCoolTime -= DT;
-		if (m_fCurCoolTime <= 0)
-		{
-			m_fCoolRatio = 0.0f;  // 쿨타임 종료, 0 설정
-			m_bSkillUse = false; // 다음 사용을 위해 초기화
-		}
-		else
-			m_fCoolRatio = m_fCurCoolTime / m_fCooldownTime;// 쿨타임 중, 쿨타임의 진행 비율을 계산하여 설정
-	}
-	//MeshRender()->SetMaterial(CResMgr::GetInst()->FindRes<CMaterial>(L"material\\Jinx_E.mtrl"), 0);
 	MeshRender()->GetMaterial(0)->SetScalarParam(FLOAT_0, &m_fCoolRatio);
 
-	if (m_bSkillUse)
+
+	//=============폰트출력===============
+	if (UICamera && m_SkillType != COOL_DOWN_TYPE::PASSIVE && m_bSkillUse)
 	{
-		//=============폰트출력===============
-		if (UICamera && m_SkillType != COOL_DOWN_TYPE::PASSIVE)
+		float Timer = m_fCurCoolTime;
+		int floor = static_cast<int>(std::floor(fabsf(Timer + 1)));
+
+		tFont Font3 = {};
+		Font3.wInputText = to_wstring(floor); //레벨 폰트
+		Font3.fontType = FONT_TYPE::RIX_KOR_L;
+		Font3.fFontSize = 16.5;
+		Font3.iFontColor = FONT_RGBA(252, 252, 250, 255);
+
+		switch ((UINT)m_SkillType)
 		{
-			float Timer = m_fCurCoolTime;
-			int floor = static_cast<int>(std::floor(fabsf(Timer + 1)));
-
-			tFont Font3 = {};
-			Font3.wInputText = to_wstring(floor); //레벨 폰트
-			Font3.fontType = FONT_TYPE::RIX_KOR_L;
-			Font3.fFontSize = 16.5;
-			Font3.iFontColor = FONT_RGBA(252, 252, 250, 255);
-
-			switch ((UINT)m_SkillType)
-			{
-			case ((UINT)COOL_DOWN_TYPE::Q):
-				Font3.vDisplayPos = Vec2(670, 915);
-				break;
-			case ((UINT)COOL_DOWN_TYPE::W):
-				Font3.vDisplayPos = Vec2(722, 915);
-				break;
-			case ((UINT)COOL_DOWN_TYPE::E):
-				Font3.vDisplayPos = Vec2(773, 915);
-				break;
-			case ((UINT)COOL_DOWN_TYPE::R):
-				Font3.vDisplayPos = Vec2(824, 915);
-				break;
-			case ((UINT)COOL_DOWN_TYPE::SPELL_D):
-			{
-				Font3.vDisplayPos = Vec2(872, 914);
-				Font3.fFontSize = 10.5f;
-			}
-				break;
-			case ((UINT)COOL_DOWN_TYPE::SPELL_F):
-			{
-				Font3.vDisplayPos = Vec2(910, 914);
-				Font3.fFontSize = 10.5f;
-			}
-				break;
-			}
-
-			UICamera->AddText(FONT_DOMAIN::MASK, Font3);
+		case ((UINT)COOL_DOWN_TYPE::Q):
+			Font3.vDisplayPos = Vec2(670, 915);
+			break;
+		case ((UINT)COOL_DOWN_TYPE::W):
+			Font3.vDisplayPos = Vec2(722, 915);
+			break;
+		case ((UINT)COOL_DOWN_TYPE::E):
+			Font3.vDisplayPos = Vec2(773, 915);
+			break;
+		case ((UINT)COOL_DOWN_TYPE::R):
+			Font3.vDisplayPos = Vec2(824, 915);
+			break;
+		case ((UINT)COOL_DOWN_TYPE::SPELL_D):
+		{
+			Font3.vDisplayPos = Vec2(872, 914);
+			Font3.fFontSize = 10.5f;
+		}
+		break;
+		case ((UINT)COOL_DOWN_TYPE::SPELL_F):
+		{
+			Font3.vDisplayPos = Vec2(910, 914);
+			Font3.fFontSize = 10.5f;
+		}
+		break;
 		}
 
+		UICamera->AddText(FONT_DOMAIN::MASK, Font3);
 	}
 }
 
--- Project/Script/CExpRatioUIScript.cpp
+++ Project/Script/CExpRatioUIScript.cpp
@@ -2,6 +2,8 @@
 #include "CExpRatioUIScript.h"
 #include <Engine\define.h>
 #include <Engine\CKeyMgr.h>
+#include "CUnitScript.h"
+#include "CChampionScript.h"
 
 void CExpRatioUIScript::begin()
 {
@@ -10,14 +12,15 @@ void CExpRatioUIScript::begin()
 void CExpRatioUIScript::tick()
 {
 	//------µð¹ö±ë¿ë-------------
-	if (KEY_PRESSED(KEY::P))
-		m_fCurrentExp += DT * 50.f;
+	//if (KEY_PRESSED(KEY::P))
+	//	m_fCurrentExp += DT * 50.f;
 
-	if (KEY_PRESSED(KEY::O))
-		m_fCurrentExp -= DT * 50.f;
+	//if (KEY_PRESSED(KEY::O))
+	//	m_fCurrentExp -= DT * 50.f;
 	//--------------------------------
-	
-	m_fCurrentExpRatio = m_fCurrentExp / m_fLevelTotalExp;
+	CChampionScript* ChampInfo = CSendServerEventMgr::GetInst()->GetMyPlayer()->GetScript<CChampionScript>();
+	float EXP = ChampInfo->GetExp(); 
+	m_fCurrentExpRatio = EXP / m_fLevelTotalExp;
 
 	if (m_fCurrentExp >= m_fLevelTotalExp)
 		m_fCurrentExp = m_fLevelTotalExp;
@@ -33,7 +36,7 @@ void CExpRatioUIScript::BeginOverlap(CCollider2D* _Other)
 
 CExpRatioUIScript::CExpRatioUIScript()
 	:CScript((UINT)SCRIPT_TYPE::EXPRATIOUISCRIPT)
-	, m_fLevelTotalExp(100.f)
+	, m_fLevelTotalExp(10.f)
 	, m_fCurrentExp(100.f)
 {
 
