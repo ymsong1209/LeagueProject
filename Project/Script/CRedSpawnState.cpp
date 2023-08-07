7f4acf197b8afe24ebc34ad1f1ed6ebd0d1e1f4a
ymsong <ssupersam13@gmail.com>
2023-08-07 18:22:02 +0900

    fix : red 동일 component 존재 hotfix

--- Project/Script/CRedSpawnState.cpp
+++ Project/Script/CRedSpawnState.cpp
@@ -31,16 +31,21 @@ void CRedSpawnState::Exit()
 	script->SetAggroRange(75.f);
 	script->SetAttackRange(45.f);
 
-	GetOwner()->AddComponent(new CCollider3D);
-	GetOwner()->Collider3D()->SetAbsolute(true);
-	GetOwner()->Collider3D()->SetDrawCollision(false);
-	GetOwner()->Collider3D()->SetOffsetScale(Vec3(65.f, 65.f, 65.f));
+	if (GetOwner()->GetComponent(COMPONENT_TYPE::COLLIDER3D) == nullptr) {
+		GetOwner()->AddComponent(new CCollider3D);
+		GetOwner()->Collider3D()->SetAbsolute(true);
+		GetOwner()->Collider3D()->SetDrawCollision(false);
+		GetOwner()->Collider3D()->SetOffsetScale(Vec3(65.f, 65.f, 65.f));
+	}
 
-	GetOwner()->AddComponent(new CCollider2D);
-	GetOwner()->Collider2D()->SetAbsolute(true);
-	GetOwner()->Collider2D()->SetCollider2DType(COLLIDER2D_TYPE::CIRCLE);
-	GetOwner()->Collider2D()->SetOffsetRot(Vec3(XMConvertToRadians(90.f), 0.f, 0.f));
-	GetOwner()->Collider2D()->SetOffsetScale(Vec2(80.f, 80.f));
+	if (GetOwner()->GetComponent(COMPONENT_TYPE::COLLIDER2D) == nullptr) {
+		GetOwner()->AddComponent(new CCollider2D);
+		GetOwner()->Collider2D()->SetAbsolute(true);
+		GetOwner()->Collider2D()->SetCollider2DType(COLLIDER2D_TYPE::CIRCLE);
+		GetOwner()->Collider2D()->SetOffsetRot(Vec3(XMConvertToRadians(90.f), 0.f, 0.f));
+		GetOwner()->Collider2D()->SetOffsetScale(Vec2(80.f, 80.f));
+	}
+	
 
 	CJungleSpawnState::Exit();
 }
