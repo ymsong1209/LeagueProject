b48392cfbd1e68292883bf4c669ae5602c64cbc9
Rehpe <rehpe1994@gmail.com>
2023-08-08 16:22:54 +0900

    fix: 미니언 프리팹에 Collider2D 추가, AttackRange 없는 버전의 프리팹 추가

--- Project/Script/CChampionScript.cpp
+++ Project/Script/CChampionScript.cpp
@@ -146,8 +146,7 @@ bool CChampionScript::CheckDeath()
 
 void CChampionScript::CheckStatus()
 {
-	//m_fHP += 0.5f * DT;
-	m_fHP += 0.1f * DT;
+	m_fHP += 0.5f * DT;
 	m_fMP += 5.0f * DT;
 
 	if (m_fHP > m_fMaxHP)
