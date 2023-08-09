7bede09f710871771e2fc9dcf86d253275002b2b
ssongting <49968982+ssongting@users.noreply.github.com>
2023-08-10 00:53:50 +0900

    teap: 디버깅용 체력바 수치를 다시 기본값으로.

--- Project/Script/CTurretHPUIScript.cpp
+++ Project/Script/CTurretHPUIScript.cpp
@@ -84,8 +84,8 @@ void CTurretHPUIScript::tick()
 
 		//----------HP------------
 		////======디버깅용=========
-		m_fCurHP = 570.f;
-		m_fTotalHP = 1000.f;
+		//m_fCurHP = 570.f;
+		//m_fTotalHP = 1000.f;
 		////====================
 		m_fRatio = m_fCurHP / m_fTotalHP;
 		if (m_fCurHP >= m_fTotalHP)
@@ -94,29 +94,6 @@ void CTurretHPUIScript::tick()
 		if (m_fCurHP <= 0.f)
 			m_fCurHP = 0.f;
 		//-------------------------
-		//Vec3 Pos = ParentObj->Transform()->GetRelativePos();
-		//CCamera* MainCam = CRenderMgr::GetInst()->GetMainCam();
-		//Matrix viewmat = MainCam->GetViewMat();
-		//Matrix projmat = MainCam->GetProjMat();
-		//Matrix MatViewProj = viewmat * projmat;
-
-		//// 4D 벡터로 변환 (w 요소를 1로 설정)
-		//Vec4 Pos4 = Vec4(Pos.x, Pos.y, Pos.z, 1.0f);
-		//// MatViewProj에 곱해줌
-		//Vec4 ProjPos = Pos4.Transform(Pos4, MatViewProj);
-		//// w로 나눠서 클리핑(ndc) 좌표계로 변환
-		//ProjPos /= ProjPos.w;
-		//Vec2 Resolution = CEngine::GetInst()->GetWindowResolution();  //화면 해상도를 가져옴
-		//Vec2 ObjscreenPos = ((Vec2(ProjPos.x, ProjPos.y) + Vec2(1.f, 1.f)) / Vec2(2.f, 2.f)) * Resolution;
-
-		//Vec4 ndcVec = Vec4((2.0f * ObjscreenPos.x) / Resolution.x - 1.0f, 1.0f - (2.0f * ObjscreenPos.y) / Resolution.y, 1.f, 1.f);
-
-		//Matrix viewInvMatrix = UICamera->GetViewMatInv();
-		//Matrix projInvMatrix = UICamera->GetProjMatInv();
-		//Matrix invViewProjMatrix = viewInvMatrix * projInvMatrix;
-
-		//Vec3 worldVec = XMVector3TransformCoord(ndcVec, invViewProjMatrix);
-		//Vec3 FinalPos = Vec3(worldVec.x + m_vOffsetBarPos.x, -worldVec.y + m_vOffsetBarPos.y, 700.f);
 
 		MeshRender()->GetDynamicMaterial(0)->SetScalarParam(FLOAT_0, &m_fRatio);
 	}
