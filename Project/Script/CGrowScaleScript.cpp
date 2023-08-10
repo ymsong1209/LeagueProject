69059a5fb4d569c66963763bbb197ba7c6f28aed
GeonHee <relu00123@gmail.com>
2023-08-11 03:26:14 +0900

    Fix : 누락된 파일들 업로드

--- Project/Script/CGrowScaleScript.cpp
+++ Project/Script/CGrowScaleScript.cpp
@@ -4,6 +4,8 @@
 
 CGrowScaleScript::CGrowScaleScript()
 	: CScript((UINT)SCRIPT_TYPE::GROWSCALESCRIPT)
+	, m_fScaleSpeed(100.f)
+	, m_vDirection(1.f, 0.f, 0.f)
 {
 }
 
@@ -17,10 +19,32 @@ void CGrowScaleScript::begin()
 
 void CGrowScaleScript::tick()
 {
-	if (CKeyMgr::GetInst()->GetKeyState(KEY::X) == KEY_STATE::TAP)
-	{
+	// 커지고 싶은 방향
+	m_vDirection;
 
+	// speed per sec (커져야할)
+	m_fScaleSpeed;
+
+	if (CKeyMgr::GetInst()->GetKeyState(KEY::X) == KEY_STATE::PRESSED)
+	{
+		Vec3 OriginalScale = GetOwner()->Transform()->GetRelativeScale();
+		Vec3 OriginalPos = GetOwner()->Transform()->GetRelativePos();
+
+		Vec3 NextScale = Vec3(OriginalScale.x + abs(m_vDirection.x) * m_fScaleSpeed * DT
+			, OriginalScale.y + abs(m_vDirection.y) * m_fScaleSpeed * DT
+			, OriginalScale.z + abs(m_vDirection.z) * m_fScaleSpeed * DT);
+
+
+		Vec3 NextPos = Vec3(OriginalPos.x + abs(m_vDirection.y) * m_fScaleSpeed * DT / 2.f
+			, OriginalPos.y + abs(m_vDirection.x) * m_fScaleSpeed * DT / 2.f
+			, OriginalPos.z + abs(m_vDirection.z) * m_fScaleSpeed * DT / 2.f);
+
+
+		GetOwner()->Transform()->SetRelativeScale(NextScale);
+		GetOwner()->Transform()->SetRelativePos(NextPos);
 	}
 }
+
+
 
 
--- Project/Script/CGrowScaleScript.h
+++ Project/Script/CGrowScaleScript.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <Engine\CScript.h>
- 
+
+
 class CGrowScaleScript :
     public CScript
 {
+public:
+    Vec3  m_vDirection;
+    float m_fScaleSpeed;
+
 public:
     virtual void begin() override;
     virtual void tick() override;
@@ -13,6 +18,7 @@ public:
 public:
     CGrowScaleScript();
     ~CGrowScaleScript();
-        
+
 };
+
 
