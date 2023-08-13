2cd0cd1e46ea58ee595b73fea74d9d11f49cfaee
ssongting <49968982+ssongting@users.noreply.github.com>
2023-08-13 11:16:02 +0900

    temp: 값잘못설정한거 수정

--- Project/Client/ServerEventMgr.cpp
+++ Project/Client/ServerEventMgr.cpp
@@ -36,7 +36,7 @@ void ServerEventMgr::sendtick(ClientServiceRef _service)
 	// ========================================
 	// 규칙 패킷(MovePacket) : 100ms = 1/10 sec
     // ========================================
-	if (time_diff.count() >= 16.67)  // 나중에 40으로 고침
+	if (time_diff.count() >= 100)  // 나중에 40으로 고침
     {     
         // 1. 본인 플레이어 move 패킷을 서버에 보낸다. (ObjectMove)
 		// 문제점 : 안움직일땐 move 패킷을 안보내서 LV,HP,MP 등 업데이트가 안됌...그냥 움직여라는 뜻.(나중에는 안움직일때도 패킷보내게 변경예정)
