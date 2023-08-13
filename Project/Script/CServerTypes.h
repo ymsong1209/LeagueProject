748856e70d07386a4dda09d3f3a1e6ae6149daf0
Rehpe <rehpe1994@gmail.com>
2023-08-14 01:04:37 +0900

    fix: SkillType 추가

--- Project/Script/CServerTypes.h
+++ Project/Script/CServerTypes.h
@@ -210,7 +210,9 @@ enum class DimensionType
 enum class SkillType
 {
 	NONE,
-	BASIC_ATTACK,		// 이거 취소함// 모든 종류의 평타 (미니언/정글몹의 기본공격 포함)
+	BASIC_ATTACK,			// 몹의 근접 평타
+	BASIC_RANGED_ATTACK,	// 미니언 원거리 평타
+	JINX_BASIC_ATTACK,
 	JINX_Q,
 	JINX_W,
 	JINX_E,
