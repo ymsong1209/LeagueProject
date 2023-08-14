5332ac2130e89f2e2044478da8da0545ecfc81a2
Rehpe <rehpe1994@gmail.com>
2023-08-15 01:58:15 +0900

    temp: 롤백

--- Project/Engine/CCollider2D.cpp
+++ Project/Engine/CCollider2D.cpp
@@ -36,47 +36,58 @@ CCollider2D::~CCollider2D()
 
 void CCollider2D::finaltick()
 {
+	// 충돌 회수가 음수인 경우
 	assert(0 <= m_iCollisionCount);
+	m_matColliderScale = XMMatrixIdentity();
+	m_matColliderScale = XMMatrixScaling(m_vOffsetScale.x, m_vOffsetScale.y, m_vOffsetScale.z);
+	
+	m_matColliderRot = XMMatrixIdentity();
+	m_matColliderRot = XMMatrixRotationX(m_vOffsetRot.x);
+	m_matColliderRot *= XMMatrixRotationY(m_vOffsetRot.y);
+	m_matColliderRot *= XMMatrixRotationZ(m_vOffsetRot.z);
+	
+	m_matColliderPos = XMMatrixIdentity();
+	m_matColliderPos = XMMatrixTranslation(m_vOffsetPos.x, m_vOffsetPos.y, m_vOffsetPos.z);
 
-	// 물체의 월드 행렬
-	const Matrix& matWorld = Transform()->GetWorldMat();
-	Vec3 objWorldPos = Transform()->GetRelativePos();  // 물체의 월드 위치
-	Vec3 objWorldScale = Transform()->GetRelativeScale();  // 물체의 월드 스케일
+	m_matCollider2D = XMMatrixScaling(m_vOffsetScale.x, m_vOffsetScale.y, m_vOffsetScale.z);
+	m_matCollider2D *= m_matColliderRot;
+	m_matCollider2D *= XMMatrixTranslation(m_vOffsetPos.x, m_vOffsetPos.y, m_vOffsetPos.z);
+	//크기 X 회전 X 이동 (회전은 안함)
+	const Matrix& matWorld = Transform()->GetWorldMat(); //최종 월드 행렬
 
-	if (m_bFixed) {
-		// 위치만 물체의 위치를 따르게 합니다.
-		m_matColliderPos = XMMatrixTranslation(objWorldPos.x + m_vOffsetPos.x, objWorldPos.y + m_vOffsetPos.y, objWorldPos.z + m_vOffsetPos.z);
-
-		// 충돌체의 최종 스케일을 물체의 스케일과 offset 스케일을 곱하여 계산합니다.
-		m_matColliderScale = XMMatrixScaling(objWorldScale.x * m_vOffsetScale.x, objWorldScale.y * m_vOffsetScale.y, m_vOffsetScale.z);
-
-		// 충돌체의 최종 행렬
-		m_matCollider2D = m_matColliderScale * m_matColliderPos;
-	}
-	else {
-		// 크기 * 회전 * 이동 순서로 행렬을 곱합니다.
-		m_matCollider2D = m_matColliderScale * m_matColliderRot * m_matColliderPos * matWorld;
-	}
-
-	if (m_bAbsolute) {
-		// 부모의 Scale을 없애기 위한 로직
+	if (m_bAbsolute)
+	{
+		//부모의 Scale없애기
 		Matrix matParentScaleInv = XMMatrixInverse(nullptr, Transform()->GetWorldScaleMat());
+		//Collider의 Offset * 부모 크기의 역행렬 * 부모
 		m_matCollider2D = m_matCollider2D * matParentScaleInv * matWorld;
+
 	}
+	else
+	{
+		// 충돌체 월드 * 오브젝트 월드
+		m_matCollider2D *= matWorld;
+
+		// 충돌체 scale update
+		m_matColliderScale *= Transform()->GetWorldScaleMat();
+		m_matColliderPos *= Transform()->GetWorldPosMat();
+	}
+
 
 	// DebugShape 요청
 	Vec4 vColor = Vec4(0.f, 1.f, 0.f, 1.f);
 	if (0 < m_iCollisionCount)
 		vColor = Vec4(1.f, 0.f, 0.f, 1.f);
 
-	if (m_bDrawCollision) {
+	if (m_bDrawCollision)
+	{
+		// 마지막에 0.f로 설정해 GameObject가 사라지면 Collider도 사라진다.
 		if (COLLIDER2D_TYPE::CIRCLE == m_Shape)
 			DrawDebugCircle(m_matCollider2D, vColor, 0.f);
 		else
 			DrawDebugRect(m_matCollider2D, vColor, 0.f);
 	}
 }
-
 
 
 void CCollider2D::BeginOverlap(CCollider2D* _Other)
